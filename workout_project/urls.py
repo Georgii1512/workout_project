2d100e0b62a97b1c9224da21b159335f0b2b5280
Georgii1512 <153933316+Georgii1512@users.noreply.github.com>
2025-05-10 00:16:08 +0300

    Add media and static hadling, bootsrap-5, crispy, debug-toolbar, jQuery file, Pillow, coments AUTH_PASSWORD_VALIDATORS in settings.py, index url, view and template.

--- workout_project/urls.py
+++ workout_project/urls.py
@@ -16,7 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+
+from debug_toolbar.toolbar import debug_toolbar_urls
+
+from .views import IndexView
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index')
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += debug_toolbar_urls()
+    urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
