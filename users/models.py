b8bb3511296f1629875588285e8065196136695c
Georgii1512 <153933316+Georgii1512@users.noreply.github.com>
2025-05-11 15:35:21 +0300

    Add user model and registered at admin panel.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='avatars', default='default_user_photo.png', blank=True)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        ordering = ['username']
+        verbose_name_plural = 'users'
+
+    def __str__(self):
+        return f'{self.username} - {self.uuid}'
--- workout_project/settings.py
+++ workout_project/settings.py
@@ -138,3 +138,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+
+# Users
+AUTH_USER_MODEL = 'users.User'
