42c5056f8705a36cdcd732c76db7beeaa6f07ebf
Georgii1512 <153933316+Georgii1512@users.noreply.github.com>
2025-07-18 21:59:33 +0300

    Branch initial

--- workouts/views/training_plan_views.py
+++ workouts/views/training_plan_views.py
@@ -2,12 +2,17 @@ from django.views.generic import (CreateView, DetailView, DeleteView, UpdateView
 from django.contrib.auth import mixins
 from workouts import forms, models
 from common.mixins import OwnerRequiredMixin
+from django.urls import reverse_lazy
 
 
 class TrainingPlanCreateView(mixins.LoginRequiredMixin, CreateView):
     model = models.TrainingPlan
+    template_name = 'workouts/training_plan/training_plan_create.html'
     context_object_name = 'training_plan'
     form_class = forms.TrainingPlanForm
+    success_url = reverse_lazy('workouts:training_plans_list')
+    slug_url_kwarg = 'slug'
+    slug_field = 'slug'
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
@@ -17,15 +22,28 @@ class TrainingPlanCreateView(mixins.LoginRequiredMixin, CreateView):
 
 class TrainingPlanDeleteView(OwnerRequiredMixin, DeleteView):
     model = models.TrainingPlan
+    template_name = 'workouts/training_plan/training_plan_delete.html'
     context_object_name = 'training_plan'
+    success_url = reverse_lazy('workouts:training_plans_list')
+    slug_url_kwarg = 'slug'
+    slug_field = 'slug'
 
 
 class TrainingPlanUpdateView(OwnerRequiredMixin, UpdateView):
     model = models.TrainingPlan
+    template_name = 'workouts/training_plan/training_plan_update.html'
     context_object_name = 'training_plan'
     form_class = forms.TrainingPlanForm
+    slug_url_kwarg = 'slug'
+    slug_field = 'slug'
+
+    def get_success_url(self):
+        return reverse_lazy('workouts:training_plan_detail', kwargs={'slug': self.get_object().slug})
 
 
 class TrainingPlanDetailView(OwnerRequiredMixin, DetailView):
     model = models.TrainingPlan
+    template_name = 'workouts/training_plan/training_plan_detail.html'
     context_object_name = 'training_plan'
+    slug_url_kwarg = 'slug'
+    slug_field = 'slug'
